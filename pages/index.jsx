8b6bd87311818609182205671e5f17a946eb3344
SeungJin051 <seungjin051@gmail.com>
2024-08-13 04:06:21 +0900

    Create index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import Hero from '@/components/home/Hero';
+
+export default function Home() {
+  return (
+    <div>
+      <section className="text-gray-600 body-font">
+        <section className="flex flex-col items-center min-h-screen text-gray-600 body-font">
+          <div className="container flex flex-col items-center px-5 py-24 mx-auto md:flex-row">
+            <Hero />
+          </div>
+        </section>
+      </section>
+    </div>
+  );
+}
