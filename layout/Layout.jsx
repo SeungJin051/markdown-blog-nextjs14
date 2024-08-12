303013fd2159fcdcdc500509db1d5f0c476c7da3
SeungJin051 <seungjin051@gmail.com>
2024-08-13 04:05:02 +0900

    Create Layout.jsx

---
+++ layout/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+export default function Layout({ children }) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow">{children}</main>
+      <Footer />
+    </div>
+  );
+}
