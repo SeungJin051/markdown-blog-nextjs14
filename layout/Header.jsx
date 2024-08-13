300cd1662fff8c1b7ddac5171ea1ffc30bea0e8a
SeungJin051 <seungjin051@gmail.com>
2024-08-14 03:14:17 +0900

    다크모드

--- layout/Header.jsx
+++ layout/Header.jsx
@@ -1,11 +1,16 @@
+import DarkMode from '@/components/home/DarkMode';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 export default function Header() {
   return (
     <header className="text-gray-600 border-b-2 body-font">
       <div className="container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row">
-        <a className="flex items-center mb-4 font-medium text-gray-900 title-font md:mb-0">
+        <Link
+          href="/"
+          className="flex items-center mb-4 md:mb-0 dark:text-gray-400"
+        >
           <Image
             src="/GDSCUTM-Bracket.png"
             alt="GDSCUTM-Bracket.png"
@@ -14,29 +19,46 @@ export default function Header() {
           />
 
           <span className="ml-3 text-xl">GDSC DEU</span>
-        </a>
+        </Link>
         <nav className="flex flex-wrap items-center justify-center text-base md:ml-auto md:mr-auto">
-          <a className="mr-5 hover:text-gray-900">About Us</a>
-          <a className="mr-5 hover:text-gray-900">Notice</a>
-          <a className="mr-5 hover:text-gray-900">Blog</a>
-          <a className="mr-5 hover:text-gray-900">Study</a>
-          <a className="mr-5 hover:text-gray-900">Event</a>
-          <a className="mr-5 hover:text-gray-900">Member</a>
-        </nav>
-        <button className="inline-flex items-center px-3 py-1 mt-4 text-base bg-gray-100 border-0 rounded focus:outline-none hover:bg-gray-200 md:mt-0">
-          Button
-          <svg
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            className="w-4 h-4 ml-1"
-            viewBox="0 0 24 24"
+          <Link
+            href="/"
+            className="mr-5 hover:text-gray-900 dark:text-gray-400"
           >
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </button>
+            About Us
+          </Link>
+          <Link
+            href="/"
+            className="mr-5 hover:text-gray-900 dark:text-gray-400"
+          >
+            Notice
+          </Link>
+          <Link
+            href="/"
+            className="mr-5 hover:text-gray-900 dark:text-gray-400"
+          >
+            Blog
+          </Link>
+          <Link
+            href="/study"
+            className="mr-5 hover:text-gray-900 dark:text-gray-400"
+          >
+            Study
+          </Link>
+          <Link
+            href="/"
+            className="mr-5 hover:text-gray-900 dark:text-gray-400"
+          >
+            Event
+          </Link>
+          <Link
+            href="/"
+            className="mr-5 hover:text-gray-900 dark:text-gray-400"
+          >
+            Member
+          </Link>
+        </nav>
+        <DarkMode />
       </div>
     </header>
   );
