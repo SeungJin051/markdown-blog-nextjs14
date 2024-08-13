d31cb6c67beeb1cd6688aee3ffcd27d20c293d8c
SeungJin051 <seungjin051@gmail.com>
2024-08-14 03:15:06 +0900

    Update Layout.jsx

--- layout/Layout.jsx
+++ layout/Layout.jsx
@@ -4,7 +4,7 @@ import Footer from './Footer';
 
 export default function Layout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-primary">
       <Header />
       <main className="flex-grow">{children}</main>
       <Footer />
