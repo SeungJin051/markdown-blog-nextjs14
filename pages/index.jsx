4ed6e8bc230400ec400b764a9ad1907f5230030d
SeungJin051 <seungjin051@gmail.com>
2024-08-15 04:28:59 +0900

    새로운 페이지 Things 추가

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,15 @@
 import Hero from '@/components/home/Hero';
+import Things from '@/components/home/Things';
 
 export default function Home() {
   return (
     <div>
       <section className="text-gray-600 body-font">
         <section className="flex flex-col items-center min-h-screen text-gray-600 body-font">
-          <div className="container flex flex-col items-center px-5 py-24 mx-auto md:flex-row">
+          <div className="container flex flex-col items-center px-5 py-24 pb-0 mx-auto md:flex-row">
             <Hero />
           </div>
+          <Things />
         </section>
       </section>
     </div>
