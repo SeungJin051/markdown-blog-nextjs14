43ea00e89c934f7d85107b98ef66976a10c60ec4
SeungJin051 <seungjin051@gmail.com>
2024-08-15 04:29:06 +0900

    images url 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'www.notion.so',
+      'prod-files-secure.s3.us-west-2.amazonaws.com',
+      'developer.android.com',
+      'i.pinimg.com',
+      'programmers.co.kr',
+      'image.yes24.com',
+      'data:image',
+      'littledeep.com',
+      'images.velog.io',
+      'media.vlpt.us',
+      'images.unsplash.com',
+      'gaussian37.github.io',
+      'www.juicymedia.co.uk',
+    ],
+  },
 };
 
 export default nextConfig;
