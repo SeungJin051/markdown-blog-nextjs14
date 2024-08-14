da83d2ce972522d0153ec264e9e30b6841eb317a
SeungJin051 <seungjin051@gmail.com>
2024-08-15 04:29:28 +0900

    Create Things.jsx

---
+++ components/home/Things.jsx
@@ -0,0 +1,194 @@
+import React from 'react';
+
+export default function Things() {
+  return (
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-wrap -m-4">
+          <div className="p-4 lg:w-1/3">
+            <div className="relative h-full px-8 pt-16 pb-24 overflow-hidden text-center bg-gray-100 bg-opacity-75 rounded-lg">
+              <h2 className="mb-5 text-xs font-medium tracking-widest text-gray-400 title-font">
+                CATEGORY
+              </h2>
+              <h1 className="mb-3 text-xl font-medium text-gray-900 title-font sm:text-2xl">
+                📖 스터디
+              </h1>
+              <p className="mb-3 leading-relaxed">
+                본인이 배우고 싶은 분야에 대해 같이 공부할 사람을 찾고, 학습의
+                지속 가능성을 높이며 함께 지식을 습득합니다.
+              </p>
+              <a className="inline-flex items-center text-indigo-500">
+                Learn More
+                <svg
+                  className="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+              <div className="absolute bottom-0 left-0 flex justify-center w-full py-4 mt-2 leading-none text-center">
+                <span className="inline-flex items-center py-1 pr-3 mr-3 text-sm leading-none text-gray-400 border-r-2 border-gray-200">
+                  <svg
+                    className="w-4 h-4 mr-1"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                    <circle cx="12" cy="12" r="3"></circle>
+                  </svg>
+                  1.2K
+                </span>
+                <span className="inline-flex items-center text-sm leading-none text-gray-400">
+                  <svg
+                    className="w-4 h-4 mr-1"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                  </svg>
+                  6
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="p-4 lg:w-1/3">
+            <div className="relative h-full px-8 pt-16 pb-24 overflow-hidden text-center bg-gray-100 bg-opacity-75 rounded-lg">
+              <h2 className="mb-5 text-xs font-medium tracking-widest text-gray-400 title-font">
+                CATEGORY
+              </h2>
+              <h1 className="mb-3 text-xl font-medium text-gray-900 title-font sm:text-2xl">
+                🏆 성과 공유회
+              </h1>
+              <p className="mb-3 leading-relaxed">
+                한 학기 동안 진행한 스터디에 대해서 다른 사람들에게 성과를
+                자랑하는 자리입니다. 다른 스터디의 발표를 듣고 피드백을 제공해
+                주세요.
+              </p>
+              <a className="inline-flex items-center text-indigo-500">
+                Learn More
+                <svg
+                  className="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+              <div className="absolute bottom-0 left-0 flex justify-center w-full py-4 mt-2 leading-none text-center">
+                <span className="inline-flex items-center py-1 pr-3 mr-3 text-sm leading-none text-gray-400 border-r-2 border-gray-200">
+                  <svg
+                    className="w-4 h-4 mr-1"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                    <circle cx="12" cy="12" r="3"></circle>
+                  </svg>
+                  1.2K
+                </span>
+                <span className="inline-flex items-center text-sm leading-none text-gray-400">
+                  <svg
+                    className="w-4 h-4 mr-1"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                  </svg>
+                  6
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="p-4 lg:w-1/3">
+            <div className="relative h-full px-8 pt-16 pb-24 overflow-hidden text-center bg-gray-100 bg-opacity-75 rounded-lg">
+              <h2 className="mb-5 text-xs font-medium tracking-widest text-gray-400 title-font">
+                CATEGORY
+              </h2>
+              <h1 className="mb-3 text-xl font-medium text-gray-900 title-font sm:text-2xl">
+                🎙️ 정기 세션
+              </h1>
+              <p className="mb-3 leading-relaxed">
+                매달 2회 진행되며, 시험이 있는 달에는 1번 진행됩니다. 발표자로
+                세션에 참가시 GDSC Community에 명단이 올라갑니다. 가벼운 주제도
+                괜찮으며, 모두가 알고 있는 지식도 괜찮습니다.
+              </p>
+              <a className="inline-flex items-center text-indigo-500">
+                Learn More
+                <svg
+                  className="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+              <div className="absolute bottom-0 left-0 flex justify-center w-full py-4 mt-2 leading-none text-center">
+                <span className="inline-flex items-center py-1 pr-3 mr-3 text-sm leading-none text-gray-400 border-r-2 border-gray-200">
+                  <svg
+                    className="w-4 h-4 mr-1"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                    <circle cx="12" cy="12" r="3"></circle>
+                  </svg>
+                  1.2K
+                </span>
+                <span className="inline-flex items-center text-sm leading-none text-gray-400">
+                  <svg
+                    className="w-4 h-4 mr-1"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                  </svg>
+                  6
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
