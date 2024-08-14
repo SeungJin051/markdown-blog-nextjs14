5c1907753815ec4d53ec364f0493c20214190e17
SeungJin051 <seungjin051@gmail.com>
2024-08-15 04:28:36 +0900

    스터디 API처리

--- pages/study/index.jsx
+++ pages/study/index.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { TOKEN, DATABASE_ID } from '@/config';
+import StudyItem from '@/components/studys/StudyItem';
 
 export default function Studys({ studys }) {
-  // console.log(studys);
-  const { id } = studys; // 디스트럭처링
-
   return (
-    <div>
-      <h1>총 스터디 : {studys.results.length}</h1>
+    <div className="grid grid-cols-1 gap-2 p-10 pt-10 dark:bg-customDarkBg md:grid-cols-2 xl:grid-cols-3">
+      <h1 className="mb-0 text-4xl font-bold m-7 col-span-full">
+        스터디
+        <span className="ml-3 text-2xl col-span-full">
+          {studys.results.length}
+        </span>
+      </h1>
+      <span className="mb-20 text-2xl ml-7 col-span-full">
+        더 나은 우리를 만드는 스터디
+      </span>
       {studys.results.map(study => (
-        <h1 key={id}>{study.properties.Name.title[0]?.plain_text}</h1>
+        <StudyItem key={study.id} data={study} />
       ))}
     </div>
   );
@@ -29,7 +35,7 @@ export async function getStaticProps() {
       sorts: [
         {
           property: 'Name',
-          direction: 'ascending',
+          direction: 'descending',
         },
       ],
       page_size: 100,
