4db6d5d13fb570ba9558823c71a07dd2e291c4ac
SeungJin051 <seungjin051@gmail.com>
2024-08-20 00:05:59 +0900

    블로그 글 목록 UI 수정

--- components/post/PostCard.jsx
+++ components/post/PostCard.jsx
@@ -15,11 +15,12 @@ function PostCard({ data }) {
   return (
     <Link
       href={`/blog/${slug}`}
-      className="block m-5 overflow-hidden transition-transform duration-300 transform bg-white rounded-lg shadow-lg hover:scale-105 hover:shadow-2xl dark:bg-customDarkStudyBg dark:hover:bg-gray-800 hover:bg-gray-100"
+      className="block m-5 overflow-hidden transition-transform duration-300 transform bg-white rounded-lg shadow-lg hover:scale-y-105 hover:shadow-2xl dark:bg-customDarkStudyBg dark:hover:bg-gray-800 hover:bg-gray-100"
+      style={{ transformOrigin: 'bottom' }}
     >
       <div className="relative w-full h-60 aspect-w-16 aspect-h-9">
         <Image
-          src={data.thumbnail || '/default-thumbnail.jpg'} // 기본 이미지 설정
+          src={data.thumbnail || '/GDSC_Dong-Eui_University_Vertical_color.png'} // 기본 이미지 설정
           alt={data.title || 'Post Thumbnail'}
           fill
           style={{ objectFit: 'cover' }}
@@ -29,11 +30,28 @@ function PostCard({ data }) {
         <h2 className="text-xl font-semibold text-black dark:text-white">
           {data.title}
         </h2>
-        <p className="mb-2 text-lg text-gray-700 dark:text-gray-300">
-          {data.author}
-        </p>
+        <div>
+          <div className="mt-2 mb-2">
+            {Array.isArray(data.tags) && data.tags.length > 0 ? (
+              data.tags.slice(0, 3).map((tag, index) => (
+                <button
+                  key={index}
+                  className="px-3 py-1 m-1 bg-slate-200 drak:bg-gray-600 rounded-2xl"
+                >
+                  {tag}
+                </button>
+              ))
+            ) : (
+              <div className="p-5 m-1"></div>
+            )}
+          </div>
+        </div>
         <p className="mt-2 text-sm text-gray-500 dark:text-gray-100">
           {formattedDate}
+        </p>
+        <hr className="mt-3 mb-3 border-gray-400" />
+        <p className="ml-3 text-sm text-gray-700 dark:text-gray-300">
+          by {data.author}
         </p>
       </div>
     </Link>
--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,4 +1,3 @@
-// /pages/blog/index.js
 import React from 'react';
 import { getAllPostFile, getMetadata } from '@/config/BlogEngine'; // 서버 사이드 전용 모듈
 import PostCard from '@/components/post/PostCard';
@@ -24,17 +23,19 @@ export async function getStaticProps() {
 
 export default function Blog({ posts }) {
   return (
-    <div className="grid grid-cols-1 gap-2 p-10 pt-10 dark:bg-customDarkBg md:grid-cols-2 xl:grid-cols-3">
-      <h1 className="mb-0 text-4xl font-bold m-7 col-span-full">
-        스터디
-        <span className="ml-3 text-2xl col-span-full"></span>
-      </h1>
-      <span className="mb-20 text-2xl ml-7 col-span-full">
-        더 나은 우리를 만드는 스터디
-      </span>
-      {posts.map(post => (
-        <PostCard key={post.fileName} data={post} />
-      ))}
+    <div className="container flex flex-col items-center justify-center mx-auto">
+      <div className="grid grid-cols-1 gap-2 p-10 pt-10 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+        <h1 className="mb-0 text-4xl font-bold m-7 col-span-full">
+          📝 Blog
+          <span className="ml-3 text-2xl col-span-full">{posts.length}</span>
+        </h1>
+        <span className="mb-10 text-2xl ml-7 col-span-full">
+          블로그를 통해 보는 GDSC-DEU의 노력과 성취
+        </span>
+        {posts.map(post => (
+          <PostCard key={post.fileName} data={post} />
+        ))}
+      </div>
     </div>
   );
 }
