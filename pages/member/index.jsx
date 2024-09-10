f898c5267593a15f3815fd034a06cbfe9e66f443
SeungJin051 <seungjin051@gmail.com>
2024-09-11 02:45:35 +0900

    GDSC 멤버 리스트 출력
    노션 API & 깃허브 API
    (정렬 문제 수정 필요)

--- pages/member/index.jsx
+++ pages/member/index.jsx
@@ -1,11 +1,8 @@
 import Members from '@/components/members/Members';
+import { TOKEN, MEMBER_DATABASE_ID } from '@/config';
 import React from 'react';
 
-// 1. Notion API를 통해서 정보를 가져옴(DB)
-// 2. 필수 정보 뿌려주기(이름, Role, 활동기수, 학년, 학과, 상태, 깃허브)
-// Role, 상태 -> 값 별로 CSS
-// 3. User 깃허브 "avatar_url" 가져오기 위해서 github API 사용
-export default function Member() {
+export default function Member({ members }) {
   return (
     <section className="text-gray-600 body-font">
       <div className="container px-5 py-24 mx-auto">
@@ -15,8 +12,47 @@ export default function Member() {
           </h1>
           <p className="mx-auto text-base leading-relaxed lg:w-2/3">asdfasdf</p>
         </div>
-        <Members />
+        <div className="container px-5 mx-auto">
+          <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-5">
+            {members.results.map(member => (
+              <Members key={member.id} data={member} />
+            ))}
+          </div>
+        </div>
       </div>
     </section>
   );
+}
+
+export async function getStaticProps() {
+  const options = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Notion-Version': '2022-06-28',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${TOKEN}`,
+      body: JSON.stringify({
+        sorts: [
+          {
+            timestamp: 'last_edited_time',
+            direction: 'descending',
+          },
+        ],
+        page_size: 100,
+      }),
+    },
+  };
+
+  const res = await fetch(
+    `https://api.notion.com/v1/databases/${MEMBER_DATABASE_ID}/query`,
+    options,
+  );
+
+  const members = await res.json();
+
+  return {
+    props: { members },
+    revalidate: 300,
+  };
 }
