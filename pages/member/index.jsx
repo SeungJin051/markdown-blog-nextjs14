7b2e9a4bb0cbafd288d52b47cb2daa3f46508338
SeungJin051 <seungjin051@gmail.com>
2024-09-11 13:18:58 +0900

    member Tailwind 수정

--- pages/member/index.jsx
+++ pages/member/index.jsx
@@ -3,14 +3,21 @@ import { TOKEN, MEMBER_DATABASE_ID } from '@/config';
 import React from 'react';
 
 export default function Member({ members }) {
+  // 데이터 유효성 검사
+  if (!members || !members.results) {
+    return <h1>데이터를 불러오지 못했습니다.</h1>;
+  }
+
   return (
     <section className="text-gray-600 body-font">
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-col w-full mb-20 text-center">
-          <h1 className="mb-4 text-2xl font-medium text-gray-900 title-font">
+          <h1 className="mb-4 text-4xl text-gray-900 title-font dark:text-white">
             OUR Members
           </h1>
-          <p className="mx-auto text-base leading-relaxed lg:w-2/3">asdfasdf</p>
+          <p className="mx-auto text-2xl leading-relaxed lg:w-2/3 dark:text-white">
+            GDSC가 함께 만들어가는 지속가능한 미래
+          </p>
         </div>
         <div className="container px-5 mx-auto">
           <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-5">
